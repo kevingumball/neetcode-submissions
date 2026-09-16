c6d58856fdc19fc6398d504e8525a9510e0a0ed6
kevingumball <135990484+kevingumball@users.noreply.github.com>
2026-09-15 18:10:06 -0700

    Add: cheapest-flight-path - submission-4

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-4.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        edges = collections.defaultdict(list)
+        for com, to, price in flights:
+            edges[com].append((price, to))
+        
+        minHeap = [(0, src, 0)]
+        visit = set()
+        res = 0
+        
+
+        while minHeap:
+            cost1, plc1, t = heapq.heappop(minHeap)
+            if t - 1 > k:
+                continue
+            if (plc1, cost1) in visit:
+                continue
+            visit.add((plc1, cost1))
+            if plc1 == dst:
+                return cost1
+            for cost2, plc2 in edges[plc1]:
+                if (plc2, cost1 + cost2) not in visit:
+                    heapq.heappush(minHeap, (cost1 + cost2, plc2, t + 1))
+        return -1
+
+
+        
